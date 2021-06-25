7ab2eecbee57478f3a4edd5902c12e412a7e3e87
kiocam <jasoncameron@Jasons-MacBook-Pro.local>
2021-06-25 14:38:47 -0400

    update header

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,8 +1,12 @@
 <template>
   <v-app>
-    <v-app-bar absolute color="white" elevate-on-scroll>
+    <v-app-bar class="pa-6" absolute color="white" elevate-on-scroll>
       <v-app-bar-title>{{title}}</v-app-bar-title>
-
+       <v-tabs align-with-title>
+          <v-tab>Tab 1</v-tab>
+          <v-tab>Tab 2</v-tab>
+          <v-tab>Tab 3</v-tab>
+        </v-tabs>
     </v-app-bar>
       <nuxt />
   </v-app>
