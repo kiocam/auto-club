a165cc2c5880e2ad1c7fb39683e411e03a5f49a4
kiocam <jasoncameron@Jasons-MacBook-Pro.local>
2021-06-25 13:08:39 -0400

    removal of unneccessry files, and initial project setup

---
+++ layouts/default.vue
@@ -0,0 +1,23 @@
+<template>
+  <v-app>
+    <v-app-bar absolute color="white" elevate-on-scroll>
+      <v-app-bar-title>{{title}}</v-app-bar-title>
+
+    </v-app-bar>
+      <nuxt />
+  </v-app>
+</template>
+
+<script>
+export default {
+  data () {
+    return {
+      title: 'Auto Club'
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
